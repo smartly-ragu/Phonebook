bbb739aed469164f62628c6ed7740b81e0901a85
smartly-ragu <55442886+smartly-ragu@users.noreply.github.com>
2022-04-14 12:57:30 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+    </div>
+
+    <footer>
+      <div class="navbar navbar-inverse footer">
+        <div class="container-fluid">
+          <div class="copyright">
+            <a href="">Presented by AIO with reference to MR.Ragunath S.M - <?php echo date("Y"); ?></a> All rights reserved
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
